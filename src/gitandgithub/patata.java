2d5fdbb5e5c3cad5a69e8bfb7607caea2694e91a
Jaime Hidalgo <jaimehsn@gmail.com>
2019-05-17 23:34:06 +0200

    hola soy yo mismo

---
+++ src/gitandgithub/patata.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitandgithub;
+
+/**
+ *
+ * @author jaime
+ */
+public class patata {
+    private int numeroPatata;
+
+    public patata(int numeroPatata) {
+        this.numeroPatata = numeroPatata;
+    }
+
+    public int getNumeroPatata() {
+        return numeroPatata;
+    }
+
+    public void setNumeroPatata(int numeroPatata) {
+        this.numeroPatata = numeroPatata;
+    }
+    
+}
